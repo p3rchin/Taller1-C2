2f8e3a8cfb2b3cee156c12e391ddc79c0dfb9ccf
p3erchin <fcgomez@unbosque.edu.co>
2021-10-02 23:22:29 -0500

    Adding in view, controller

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -4,17 +4,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.filechooser.FileView;
 
+import co.edu.unbosque.view.FileWindowView;
 import co.edu.unbosque.view.PrincipalView;
 
 public class Controller implements ActionListener {
 
 	private PrincipalView principalView;
+	private FileWindowView fileView;
 
 	public Controller() throws Exception {
 
 		principalView = new PrincipalView();
-		
+		fileView = new FileWindowView();
+
 		assignListeners();
 	}
 
@@ -22,6 +26,10 @@ public class Controller implements ActionListener {
 
 		principalView.getMinimize().addActionListener(this);
 		principalView.getClose().addActionListener(this);
+		principalView.getButton1().addActionListener(this);
+		fileView.getMinimize().addActionListener(this);
+		fileView.getClose().addActionListener(this);
+		fileView.getReturnButton().addActionListener(this);
 	}
 
 	@Override
@@ -34,5 +42,21 @@ public class Controller implements ActionListener {
 		} else if (command.equals("WINDOWCLOSE")) {
 			System.exit(0);
 		}
+
+		if (command.equals("INFILE")) {
+			principalView.setVisible(false);
+			fileView.setVisible(true);
+			String file = fileView.openFileToSearch();
+			fileView.getTextAreaShow().setText(file);
+		} else if (command.equals("WINDOWRETURN")) {
+			fileView.setVisible(false);
+			principalView.setVisible(true);
+
+		} else if (command.equals("WINDOWMINIMIZE1")) {
+			fileView.setExtendedState(JFrame.ICONIFIED);
+		} else if (command.equals("WINDOWCLOSE1")) {
+			System.exit(0);
+		}
+
 	}
 }
--- src/co/edu/unbosque/view/FileWindowView.java
+++ src/co/edu/unbosque/view/FileWindowView.java
@@ -1,82 +1,140 @@
 package co.edu.unbosque.view;
 
+import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.Image;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
-public class PrincipalView extends JFrame {
-	
+public class FileWindowView extends JFrame {
+
 	private ImageIcon background1, sourceBackground, imageButton1, sourceButton1, imageButton2, sourceButton2,
-	imageButton3, sourceButton3, imageButton4, sourceButton4;
-	
+			imageButton3, sourceButton3, imageButton4, sourceButton4;
 	private JLabel imageBackground;
-	
-	private TypeButton button1, button2;
-	
-	private JButton minimize, close;
+	private TypeButton button1, button2, button3;
+	private JButton returnButton, minimize, close;
+	private JLabel title1;
+	private JTextArea textAreaShow;
+	private JScrollPane scrollPane;
+	private JFileChooser fileChooser;
+	String texto;
 
-	public PrincipalView() {
+	public FileWindowView() {
 
 		setTitle("Words searcher");
-		setSize(600, 300);
+		setSize(800, 500);
 		setUndecorated(true);
-		setIconImage(new ImageIcon("images/logo.png").getImage());
-		initializeComponents();
+		initialize();
 		setResizable(false);
 		setLayout(null);
 		setLocationRelativeTo(null);
-		setVisible(true);
+		setVisible(false);
+
 	}
 
-	public void initializeComponents() {
-		
+	public void initialize() {
+
+		fileChooser = new JFileChooser(); 
+
+		title1 = new JLabel("Enter the word to search");
+		title1.setBounds(260, 2, 400, 50);
+		title1.setFont(new Font("Arial", Font.CENTER_BASELINE, 22));
+		add(title1);
+
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 100, 765, 350);
+		add(scrollPane);
+
+		textAreaShow = new JTextArea();
+		textAreaShow.setEditable(false);
+		textAreaShow.setBackground(Color.white);
+		textAreaShow.setFont(new Font("Verdama", Font.ROMAN_BASELINE, 12));
+		textAreaShow.setLineWrap(true);
+		scrollPane.setViewportView(textAreaShow);
+
 		imageButton1 = new ImageIcon("images/minimize.png");
 		sourceButton1 = new ImageIcon(imageButton1.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
 		minimize = new JButton();
-		minimize.setBounds(550, 5, 20, 20);
+		minimize.setBounds(750, 5, 20, 20);
 		minimize.setBorderPainted(false);
 		minimize.setContentAreaFilled(false);
 		minimize.setFocusPainted(false);
 		minimize.setIcon(sourceButton1);
 		minimize.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		minimize.setActionCommand("WINDOWMINIMIZE");
+		minimize.setActionCommand("WINDOWMINIMIZE1");
 		add(minimize);
-		
+
 		imageButton2 = new ImageIcon("images/close.png");
 		sourceButton2 = new ImageIcon(imageButton2.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
 		close = new JButton();
-		close.setBounds(575, 5, 20, 20);
+		close.setBounds(775, 5, 20, 20);
 		close.setBorderPainted(false);
 		close.setContentAreaFilled(false);
 		close.setFocusPainted(false);
 		close.setIcon(sourceButton2);
 		close.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		close.setActionCommand("WINDOWCLOSE");
+		close.setActionCommand("WINDOWCLOSE1");
 		add(close);
 		
-		imageButton3 = new ImageIcon("images/selectFile.png");
-		sourceButton3 = new ImageIcon(imageButton3.getImage().getScaledInstance(166, 49, Image.SCALE_DEFAULT));
-		button1 = new TypeButton();
-		button1.setBounds(200, 150, 166, 49);
-		button1.setBorderPainted(false);
-		button1.setContentAreaFilled(false);
-		button1.setFocusPainted(false);
-		button1.setIcon(sourceButton3);
-		button1.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		button1.setActionCommand("INFILE");
-		add(button1);
-		
+		imageButton3 = new ImageIcon("images/previous.png");
+		sourceButton3 = new ImageIcon(imageButton3.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
+		returnButton = new JButton();
+		returnButton.setBounds(5, 5, 20, 20);
+		returnButton.setBorderPainted(false);
+		returnButton.setContentAreaFilled(false);
+		returnButton.setFocusPainted(false);
+		returnButton.setIcon(sourceButton3);
+		returnButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		returnButton.setActionCommand("WINDOWRETURN");
+		add(returnButton);
 
-		background1 = new ImageIcon(getClass().getResource("/images/background.png"));
-		sourceBackground = new ImageIcon(background1.getImage().getScaledInstance(600, 300, Image.SCALE_SMOOTH));
+		background1 = new ImageIcon(getClass().getResource("/images/background1.png"));
+		sourceBackground = new ImageIcon(background1.getImage().getScaledInstance(800, 600, Image.SCALE_SMOOTH));
 		imageBackground = new JLabel(sourceBackground);
-		imageBackground.setSize(600, 300);
+		imageBackground.setSize(800, 600);
 		add(imageBackground);
-		
+	}
+
+	public String openFileToSearch() {
+
+		String aux = "";
+		texto = "";
+
+		try {
+			/* llamamos el metodo que permite cargar la ventana */
+			fileChooser.showOpenDialog(this);
+			/* abrimos el archivo seleccionado */
+			File abre = fileChooser.getSelectedFile();
+
+			/*
+			 * recorremos el archivo, lo leemos para plasmarlo en el area de texto
+			 */
+			if (abre != null) {
+				FileReader archivos = new FileReader(abre);
+				BufferedReader lee = new BufferedReader(archivos);
+				while ((aux = lee.readLine()) != null) {
+					texto += aux + "\n";
+				}
+
+				lee.close();
+			}
+		} catch (IOException ex) {
+			JOptionPane.showMessageDialog(null, ex + "" + "\nNo se ha encontrado el archivo", "ADVERTENCIA!!!",
+					JOptionPane.WARNING_MESSAGE);
+		}
+		return texto;
 	}
 
 	/**
@@ -262,6 +320,34 @@ public class PrincipalView extends JFrame {
 	}
 
 	/**
+	 * @return the button3
+	 */
+	public TypeButton getButton3() {
+		return button3;
+	}
+
+	/**
+	 * @param button3 the button3 to set
+	 */
+	public void setButton3(TypeButton button3) {
+		this.button3 = button3;
+	}
+
+	/**
+	 * @return the returnButton
+	 */
+	public JButton getReturnButton() {
+		return returnButton;
+	}
+
+	/**
+	 * @param returnButton the returnButton to set
+	 */
+	public void setReturnButton(JButton returnButton) {
+		this.returnButton = returnButton;
+	}
+
+	/**
 	 * @return the minimize
 	 */
 	public JButton getMinimize() {
@@ -287,5 +373,75 @@ public class PrincipalView extends JFrame {
 	 */
 	public void setClose(JButton close) {
 		this.close = close;
+	}
+
+	/**
+	 * @return the title1
+	 */
+	public JLabel getTitle1() {
+		return title1;
+	}
+
+	/**
+	 * @param title1 the title1 to set
+	 */
+	public void setTitle1(JLabel title1) {
+		this.title1 = title1;
+	}
+
+	/**
+	 * @return the textAreaShow
+	 */
+	public JTextArea getTextAreaShow() {
+		return textAreaShow;
+	}
+
+	/**
+	 * @param textAreaShow the textAreaShow to set
+	 */
+	public void setTextAreaShow(JTextArea textAreaShow) {
+		this.textAreaShow = textAreaShow;
+	}
+
+	/**
+	 * @return the scrollPane
+	 */
+	public JScrollPane getScrollPane() {
+		return scrollPane;
+	}
+
+	/**
+	 * @param scrollPane the scrollPane to set
+	 */
+	public void setScrollPane(JScrollPane scrollPane) {
+		this.scrollPane = scrollPane;
+	}
+
+	/**
+	 * @return the fileChooser
+	 */
+	public JFileChooser getFileChooser() {
+		return fileChooser;
+	}
+
+	/**
+	 * @param fileChooser the fileChooser to set
+	 */
+	public void setFileChooser(JFileChooser fileChooser) {
+		this.fileChooser = fileChooser;
+	}
+
+	/**
+	 * @return the texto
+	 */
+	public String getTexto() {
+		return texto;
+	}
+
+	/**
+	 * @param texto the texto to set
+	 */
+	public void setTexto(String texto) {
+		this.texto = texto;
 	}
 }
